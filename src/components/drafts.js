242bd64bd72672a98aa5bac2c4ac81c38956efe4
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2021-05-16 23:27:30 +0200

    fetch and filter data

--- src/components/drafts.js
+++ src/components/drafts.js
@@ -1,3 +1,33 @@
-export default function Drafts() {
-    return <div>Drafts</div>
+import { useState, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    color: palevioletred;
+`;
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
+export default function () {
+    const [data, setData] = useState([]);
+    const query = useQuery();
+    const accesRestreint = query.get('acces-restreint') === 'true' ? true : query.get('acces-restreint') === 'false' ? false : undefined;
+
+    useEffect(() => fetch('https://www.data.gouv.fr/fr/datasets/r/f76f80da-ebd0-4c9a-95aa-18ff622f2450').then(x => x.json()).then(setData), []);
+
+    const filterLink = query => param => value => {
+        const clone = new URLSearchParams(query.toString());
+        clone.set(param, value);
+        return `?${clone.toString()}`;
+    }
+
+    return <>
+        <Link to={filterLink(query)('acces-restreint')('false')}>Accès restreint</Link>
+        <Link to={filterLink(query)('acces-restreint')('true')}>Accès non restreint</Link>
+        {data?.filter(x => x.acces_restreint === accesRestreint).map(x => <Div key={x.id}>{x.adresse}</Div>)}
+    </>
 }
